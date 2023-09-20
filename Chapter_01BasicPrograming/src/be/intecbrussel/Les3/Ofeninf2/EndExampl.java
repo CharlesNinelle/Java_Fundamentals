4659b487bf57d524f59ef1010235e998775b7713
CharlesNinelle <145468692+CharlesNinelle@users.noreply.github.com>
2023-09-20 15:45:18 +0200

    Add files via upload

---
+++ Chapter_01BasicPrograming/src/be/intecbrussel/Les3/Ofeninf2/EndExampl.java
@@ -0,0 +1,14 @@
+package be.intecbrussel.Les3.Ofeninf2;
+
+public class EndExampl {
+    public static void main(String[] args) {
+        int number = 5;
+        if (number>0&&number<10){
+            System.out.println("number falls in the range from 0 to 10");
+        }
+        else {
+            System.out.println("number does't falls in the range from 0 to 10");
+
+        }
+    }
+}
