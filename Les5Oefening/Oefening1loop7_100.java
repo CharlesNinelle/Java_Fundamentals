ea968247768a661bdff4d55b5d367b90a492bb20
CharlesNinelle <145468692+CharlesNinelle@users.noreply.github.com>
2023-09-21 15:51:08 +0200

    Add files via upload

---
+++ Les5Oefening/Oefening1loop7_100.java
@@ -0,0 +1,11 @@
+package Les5Oefening;
+
+public class Oefening1loop7_100 {
+    public static void main(String[] args) {
+        for (int i=100; i>=1;i--){
+            if (i%7==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ Les6/ofening5/oefening1.java
@@ -0,0 +1,13 @@
+package Les6.ofening5;
+
+public class oefening1 {
+    public static void main(String[] args) {
+        int Number01 = 253;
+        long Number02 = 2365L;
+        double doubleNumber01 = (double)
+        Number01;
+        double doubleNumber02 = (double)
+                Number02;
+
+    }
+}
