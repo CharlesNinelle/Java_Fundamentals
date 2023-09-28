009883d4c91c64f0379b879c1de1ffeaaea4bb02
CharlesNinelle <145468692+CharlesNinelle@users.noreply.github.com>
2023-09-28 15:19:57 +0200

    Add files via upload

---
+++ Oefening8/Ofening2PremetiveFloat.java
@@ -0,0 +1,23 @@
+package Les5.Oefening8;
+
+public class Ofening2PremetiveFloat {
+    public static void main(String[] args) {
+        //Create a primitive floating point variable
+        //Maak een primitieve drijvende-kommavariabele
+        Float floatVal = 5.59837f;
+
+        //Convert it to a string
+        //Convert het naar een string
+        String floatString = String.valueOf(floatVal);
+
+        //Display the length of the string
+        //Geeft de lengte van de string weer
+        int stringLength = floatString.length();
+        System.out.println("Geconverteerde string: "+floatString);
+        System.out.println("Snaarlengte: " +stringLength);
+
+
+
+
+    }
+}
