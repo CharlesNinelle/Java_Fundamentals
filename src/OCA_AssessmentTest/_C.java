c1db3d362c74f7793e6b20fc4ff902ddbaac9e0d
CharlesNinelle <145468692+CharlesNinelle@users.noreply.github.com>
2024-04-25 15:01:17 +0200

    Add files via upload

---
+++ src/OCA_AssessmentTest/_C.java
@@ -0,0 +1,11 @@
+package OCA_AssessmentTest;
+
+public class _C {
+/*private static int $;
+public static void main(String[] main) {
+String a_b;
+System.out.print($);
+System.out.print(a_b);
+}*/
+}
+
