3d5d12c0108881acf636bcef032c7559d42b4cef
CharlesNinelle <145468692+CharlesNinelle@users.noreply.github.com>
2024-04-25 15:02:18 +0200

    Add files via upload

---
+++ Oefening8/Ofening1Transformatie.java
@@ -0,0 +1,16 @@
+package Les5.Oefening8;
+
+public class Ofening1Transformatie {
+    public static void main(String[] args) {
+        //Transformatie from int to Intigeg
+        int  numberInInt= 25;
+        Integer numberInteger = numberInInt;
+
+        double  numberInDouble= 2.99;
+        Double numberDouble = numberInDouble;
+
+        char nietDeNumber = 'j';
+        Character nietNumber = nietDeNumber;
+
+    }
+}
